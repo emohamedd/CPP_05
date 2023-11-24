2d09a6bb2d8476b88ddb70bd96b521b47b9b83f0
emohamedd <a.elgaouej@gmail.com>
2023-11-24 16:33:59 +0100

    throwing the exeptions

--- ex00/Bureaucrat.cpp
+++ ex00/Bureaucrat.cpp
@@ -6,7 +6,39 @@
 /*   By: emohamed <emohamed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 13:21:40 by emohamed          #+#    #+#             */
-/*   Updated: 2023/11/22 13:21:41 by emohamed         ###   ########.fr       */
+/*   Updated: 2023/11/24 16:16:30 by emohamed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+int Bureaucrat::getGrade(){
+    return this->grade;
+}
+
+Bureaucrat::Bureaucrat(std::string const name, int grade) : name(name)
+{
+    if (grade < 1)
+    {
+        throw Bureaucrat::GradeTooHighException();
+    }
+    else if (grade > 150)
+    {
+        throw Bureaucrat::GradeTooLowException();
+    }
+    this->grade = grade;
+}
+std::string Bureaucrat::getName()
+{
+    return(this->name);
+}
+
+const char* Bureaucrat::GradeTooHighException::what() const throw()
+{
+    return ("GradeTooHigh");
+}
+const char* Bureaucrat::GradeTooLowException::what() const throw()
+{
+    return ("GradeTooLow");
+}
 
--- ex00/Bureaucrat.hpp
+++ ex00/Bureaucrat.hpp
@@ -6,7 +6,33 @@
 /*   By: emohamed <emohamed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 13:21:44 by emohamed          #+#    #+#             */
-/*   Updated: 2023/11/22 13:21:45 by emohamed         ###   ########.fr       */
+/*   Updated: 2023/11/24 16:28:37 by emohamed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+#include <iostream>
+#include <string>
+#include <stdexcept>
+
+class Bureaucrat{
+    private:
+        std::string const name;
+        int grade;
+    public:
+    // Bureaucrat();
+    Bureaucrat(std::string const name, int grade);
+    // ~Bureaucrat();
+    // Bureaucrat(Bureaucrat &object);
+    // Bureaucrat operator=(Bureaucrat&  object);
+     class GradeTooHighException : public std::exception{
+        public:
+           virtual const char* what() const throw();
+    };
+    class GradeTooLowException : public std::exception{
+        public:
+           virtual const char* what() const throw();
+    };
+    std::string getName();
+    int getGrade();
+};
